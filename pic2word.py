d1f6028eba5c742032cd2224764202fcaf94fcb2
Nicolas Riquelme <nicolas@riquelmes.com>
2023-07-11 23:27:08 +0200

    Migramos de win32com a python-docx

--- pic2word.py
+++ pic2word.py
@@ -1,62 +1,66 @@
-import win32com.client as win32
 import os
 import math
+from docx import Document
+from docx.shared import Cm
 
-wordApp = win32.gencache.EnsureDispatch('Word.Application')
-wordApp.Visible = True
-doc = wordApp.Documents.Add()
+# Creamos una instancia de Document() para crear un documento de Word
+document = Document()
 
-doc.PageSetup.RightMargin = 20
-doc.PageSetup.LeftMargin = 20
-doc.PageSetup.Orientation = win32.constants.wdOrientLandscape
-# A4 en píxeles: 595x842
-doc.PageSetup.PageWidth = 595
-doc.PageSetup.PageHeight = 842
+# Saltamos una página y creamos una tabla con dos columnas y una fila para el título
+document.add_page_break()
+table = document.add_table(rows=1, cols=2)
+table.autofit = False
+table.allow_autofit = False
 
+# Establecemos el ancho de las columnas en centímetros
+table.columns[0].width = Cm(10)
+table.columns[1].width = Cm(10)
+
+# Especificamos la carpeta de la que queremos extraer las imágenes
 my_dir = "."
-piccount = 0
 
+# Contamos la cantidad de imágenes que se insertarán
+piccount = 0
 for root, dirs, files in os.walk(my_dir):
     for filename in files:
         if filename.upper().endswith(('.PNG', '.JPG')):
             piccount += 1
-print(piccount, " images will be inserted")
+print(piccount, " imágenes serán insertadas")
 
+# Especificamos el número de columnas que deseamos en la tabla
 total_column = 2
+
+# Calculamos el número total de filas necesarias para acomodar todas las imágenes
 total_row = math.ceil(piccount / total_column) + 2
-rng = doc.Range(0, 0)
-rng.ParagraphFormat.Alignment = win32.constants.wdAlignParagraphCenter
-table = doc.Tables.Add(rng, total_row, total_column)
-table.Borders.Enable = False
-if total_column > 1:
-    table.Columns.DistributeWidth()
 
-frame_max_width = 167
-frame_max_height = 125
+# Añadimos las filas necesarias a la tabla
+for row in range(total_row):
+    table.add_row()
 
-piccount = 1
+# Reiniciamos el contador de imágenes
+piccount = 0
 
+# Iteramos a través de los archivos en la carpeta y sus subcarpetas
 for root, dirs, files in os.walk(my_dir):
     for filename in files:
         if filename.upper().endswith(('.PNG', '.JPG')):
             print(filename)
-            cell_column = (piccount % total_column + 1)
-            cell_row = (piccount // total_column + 1)
+
+            # Calculamos el número de columna y fila para la celda actual
+            cell_column = (piccount % total_column)
+            cell_row = (piccount // total_column) + 2
             print('cell_column={}, cell_row={}'.format(cell_column, cell_row))
 
-            cell_range = table.Cell(cell_row, cell_column).Range
-            cell_range.ParagraphFormat.LineSpacingRule = win32.constants.wdLineSpaceSingle
-            cell_range.ParagraphFormat.SpaceBefore = 0
-            cell_range.ParagraphFormat.SpaceAfter = 3
+            # Seleccionamos la celda actual
+            cell = table.cell(cell_row, cell_column)
 
-            current_pic = cell_range.InlineShapes.AddPicture(
-                os.path.join(root, filename))
-            width, height = (frame_max_height * frame_max_width /
-                             frame_max_height, frame_max_height)
+            # Añadimos la imagen a la celda
+            current_pic = cell.add_paragraph().add_run()
+            current_pic.add_picture(os.path.join(root, filename), width=Cm(10))
 
-            current_pic.Height = height
-            current_pic.Width = width
-
-            table.Cell(cell_row, cell_column).Range.InsertAfter(
-                "\n{}".format(filename))
+            # Añadimos el nombre del archivo sin la extensión como pie de foto
+            cell.add_paragraph(os.path.splitext(filename)[0], style='Caption')
             piccount += 1
+
+# Guardamos el documento
+document.save('images.docx')
