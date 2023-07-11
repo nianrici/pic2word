43fc7fe62a166f585b68a06d0747ce281bb94ea8
Nicolas Riquelme <nicolas@riquelmes.com>
2023-07-11 23:15:52 +0200

    Añadida la recursividad

--- pic2word.py
+++ pic2word.py
@@ -2,14 +2,10 @@ import win32com.client as win32
 import os
 import math
 
-# Creando el word
-wordApp = win32.gencache.EnsureDispatch(
-    'Word.Application')  # Abrir un word nuevo
-# Mantiene abierto el word para verificar que todo se va haciendo correctamente
+wordApp = win32.gencache.EnsureDispatch('Word.Application')
 wordApp.Visible = True
-doc = wordApp.Documents.Add()  # crear word intermedio
+doc = wordApp.Documents.Add()
 
-# Darle formato
 doc.PageSetup.RightMargin = 20
 doc.PageSetup.LeftMargin = 20
 doc.PageSetup.Orientation = win32.constants.wdOrientLandscape
@@ -17,14 +13,15 @@ doc.PageSetup.Orientation = win32.constants.wdOrientLandscape
 doc.PageSetup.PageWidth = 595
 doc.PageSetup.PageHeight = 842
 
-# Insertar tabla
-my_dir = "."  # si se va a ejecutar desde otra carpeta, cámbialo
-filenames = [f.name for f in os.scandir(
-    my_dir) if f.is_file() and f.name.upper().endswith(('.PNG', '.JPG'))]
-piccount = len(filenames)
+my_dir = "."
+piccount = 0
+
+for root, dirs, files in os.walk(my_dir):
+    for filename in files:
+        if filename.upper().endswith(('.PNG', '.JPG')):
+            piccount += 1
 print(piccount, " images will be inserted")
 
-# mostrar los nombres de las imágenes
 total_column = 2
 total_row = math.ceil(piccount / total_column) + 2
 rng = doc.Range(0, 0)
@@ -34,36 +31,32 @@ table.Borders.Enable = False
 if total_column > 1:
     table.Columns.DistributeWidth()
 
-# coger todas las imágenes de la carpeta y meterlas en el mismo documento
-frame_max_width = 167  # ancho máximo
-frame_max_height = 125  # alto máximo
+frame_max_width = 167
+frame_max_height = 125
 
 piccount = 1
 
-for filename in filenames:
-    print(filename)
-    # calcular la posición de las imágenes en las celdas
-    cell_column = (piccount % total_column + 1)
-    cell_row = (piccount // total_column + 1)
-    print('cell_column={}, cell_row={}'.format(cell_column, cell_row))
+for root, dirs, files in os.walk(my_dir):
+    for filename in files:
+        if filename.upper().endswith(('.PNG', '.JPG')):
+            print(filename)
+            cell_column = (piccount % total_column + 1)
+            cell_row = (piccount // total_column + 1)
+            print('cell_column={}, cell_row={}'.format(cell_column, cell_row))
 
-    # Dar formato a las celdas
-    cell_range = table.Cell(cell_row, cell_column).Range
-    cell_range.ParagraphFormat.LineSpacingRule = win32.constants.wdLineSpaceSingle
-    cell_range.ParagraphFormat.SpaceBefore = 0
-    cell_range.ParagraphFormat.SpaceAfter = 3
+            cell_range = table.Cell(cell_row, cell_column).Range
+            cell_range.ParagraphFormat.LineSpacingRule = win32.constants.wdLineSpaceSingle
+            cell_range.ParagraphFormat.SpaceBefore = 0
+            cell_range.ParagraphFormat.SpaceAfter = 3
 
-    # Insertar las imágenes
-    current_pic = cell_range.InlineShapes.AddPicture(
-        os.path.join(os.path.abspath(my_dir), filename))
-    width, height = (frame_max_height * frame_max_width /
-                     frame_max_height, frame_max_height)
+            current_pic = cell_range.InlineShapes.AddPicture(
+                os.path.join(root, filename))
+            width, height = (frame_max_height * frame_max_width /
+                             frame_max_height, frame_max_height)
 
-    # Modificar el tamaño de la celda
-    current_pic.Height = height
-    current_pic.Width = width
+            current_pic.Height = height
+            current_pic.Width = width
 
-    # añadir el nombre del archivo a cada celda
-    table.Cell(cell_row, cell_column).Range.InsertAfter(
-        "\n{}".format(filename))
-    piccount += 1
+            table.Cell(cell_row, cell_column).Range.InsertAfter(
+                "\n{}".format(filename))
+            piccount += 1
